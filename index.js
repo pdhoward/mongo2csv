29af74625dac09f2ef8feb2f4c4adada86a42178
pdhoward <patrick.howard@hotmail.com>
2023-02-04 16:42:04 -0600

    initial

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 ////////  	   Migration SERVER     		    ///////
 /////// convert mongo collection to csv     ///////
 //////////////////////////////////////////////////
-
+require('dotenv').config()
 const {MongoClient} = require("mongodb")
 const csv = require('csv')
 const fs = require("fs");
 //const ws = fs.createWriteStream("product.csv");
 
 // let url = "mongodb://username:password@localhost:27017/";
-let url = "mongodb+srv://xio:xio@cluster0.ra6xv.mongodb.net/openai?retryWrites=true&w=majority";
+let url = process.env.DB
 const client = new MongoClient(url);
 async function main() {
   // Use connect method to connect to the server

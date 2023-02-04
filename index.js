a167868e7a35b76f3d03f2fba03d179701b3ac81
pdhoward <patrick.howard@hotmail.com>
2023-02-04 16:44:13 -0600

    final updates

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ require('dotenv').config()
 const {MongoClient} = require("mongodb")
 const csv = require('csv')
 const fs = require("fs");
-//const ws = fs.createWriteStream("product.csv");
 
-// let url = "mongodb://username:password@localhost:27017/";
 let url = process.env.DB
 const client = new MongoClient(url);
 async function main() {
-  // Use connect method to connect to the server
+  
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db();
